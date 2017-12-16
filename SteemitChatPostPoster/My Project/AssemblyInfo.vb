e585ee2fd8f66e752d51235703a56d773c283117
moisesmcardona <moises@moises-studios.com>
2017-12-16 17:03:43 -0500

    Updated Assembly Information

--- SteemitChatPostPoster/My Project/AssemblyInfo.vb
+++ SteemitChatPostPoster/My Project/AssemblyInfo.vb
@@ -8,10 +8,10 @@ Imports System.Runtime.InteropServices
 
 ' Review the values of the assembly attributes
 
-<Assembly: AssemblyTitle("SteemitChatPostPoster")>
-<Assembly: AssemblyDescription("")>
-<Assembly: AssemblyCompany("")>
-<Assembly: AssemblyProduct("SteemitChatPostPoster")>
+<Assembly: AssemblyTitle("Steemit.Chat Post Poster")>
+<Assembly: AssemblyDescription("A software to post your Steem posts into Steemit.Chat")>
+<Assembly: AssemblyCompany("Moises Cardona")>
+<Assembly: AssemblyProduct("Steemit.Chat Post Poster")>
 <Assembly: AssemblyCopyright("Copyright ©  2017")>
 <Assembly: AssemblyTrademark("")>
 
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: AssemblyVersion("1.1.0.0")>
+<Assembly: AssemblyFileVersion("1.1.0.0")>
